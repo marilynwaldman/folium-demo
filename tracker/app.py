9591750974937f0576a14009da170af32a131c32
MarilynWaldman <mdwaldman22@gmail.com>
2021-11-28 08:28:57 -0700

    cleaned it up

--- tracker/app.py
+++ tracker/app.py
@@ -24,8 +24,8 @@ logo = img(src='./static/img/logo.png', height="50", width="50", style="margin-t
 #here we define our menu items
 
 topbar = Navbar(logo,
-                View('Home', 'main'),
-              
+                Link('IXWater','http://ixwater.com'),
+                View('Home', 'main')     
                 )
 
 # registers the "top" menubar
@@ -62,20 +62,11 @@ def nocache(view):
 def main():
   return redirect('/index.html')
 
-@app.route('/index.html', methods=['GET', 'POST'])
+@app.route('/index.html', methods=['GET'])
 def index():
   if request.method == 'GET':
     #return render_template('input.html')
     map_name = f"CenCal.html"
-    app.vars['map_path'] = os.path.join(app.root_path, 'maps/' + map_name)
-    return redirect('/tracker.html')
-  elif request.method == 'POST':
-    app.vars['location'] = request.form['location']
-    app.vars['radius'] = request.form['radius']
-    # app.vars['route'] = request.form.get('route')
-    app.vars['cache'] = request.form.get('cache')
-    escaped_location = escape_spaces(app.vars['location'])
-    map_name = f"map-{escaped_location}-{app.vars['radius']}.html"
     app.vars['map_path'] = os.path.join(app.root_path, 'maps/' + map_name)
     return redirect('/tracker.html')
 
@@ -91,74 +82,21 @@ def show_map():
 
 @app.route('/tracker.html')
 def tracker():
-  """
-  loc = geocoder.osm(app.vars.get('location'))
-  if loc.lat is not None and loc.lng is not None:
-    latlng = [loc.lat, loc.lng]
-  else:
-    return redirect('/geoerror.html')
-  """
+  
   # insist on a valid map config
   map_path = app.vars.get("map_path")
   if not map_path:
     return redirect('/error.html')
+  else:
+    return render_template('display.html')  
+  """ 
   if app.vars.get("cache") == "yes" and Path(map_path).exists():
     return render_template('display.html')
   if Path(map_path).exists():
     return render_template('display.html')  
-  else:
-    #bus_map = folium.Map(location=latlng, zoom_start=15)
-    bus_map = folium.Map(location=[37.7794,-122.4194],
-                zoom_start=6,tiles=None)
-    folium.TileLayer('cartodbpositron',name='BackGround',control=False).add_to(bus_map)
-    """
-    bus_map.add_child(folium.Marker(location=latlng,
-                                    popup=escape_apostrophes(loc.address),
-                                    icon=folium.Icon(color='blue')))
-    """
-    # Call API for bus locations
-    """
-    bus_list = get_buses(loc.lat, loc.lng, app.vars['radius'])
+  """
 
-    for bus in bus_list:
-      route_id = bus.get('RouteID')
-      trip_headsign = escape_apostrophes(bus.get('TripHeadsign'))
-      folium.features.RegularPolygonMarker(location = [bus['Lat'], bus['Lon']],
-                                           popup = f"Route {route_id} to {trip_headsign}",
-                                           number_of_sides = 3,
-                                           radius = 15,
-                                           weight = 1,
-                                           fill_opacity = 0.8,
-                                           rotation = 30).add_to(bus_map)
-    """
-    bus_map.save(map_path)
-    return render_template('display.html')
   pass
-
-def get_buses(lat, lon, radius):
-  """
-  All values passed as strings and radius in meters
-  """
-  headers = {'api_key': os.environ['WMATA_KEY']}
-
-  session = requests.Session()
-  session.mount('https://api.wmata.com',
-                requests.adapters.HTTPAdapter(max_retries = 2))
-
-  bus_endpoint = 'https://api.wmata.com/Bus.svc/json/jBusPositions'
-  
-  params = {'Lat': lat,
-            'Lon': lon,
-            'Radius': radius}
-
-  # if app.vars['route']:
-  #   params['RouteID'] = app.vars['route']
-
-  response = session.get(bus_endpoint, params = params, headers = headers)
-  if not response.status_code == 200:
-    return redirect('/apierror.html')
-  else:
-    return response.json()['BusPositions']
 
 @app.route('/error.html')
 def error():
